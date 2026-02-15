07390b2c7be22ecd84a182e57b5d6576db322543
safaloramesh <safaloramesh@gmail.com>
2026-02-15 18:33:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '', // Change this from '/' to ''
+  base: './', // Ensures assets load correctly on any port
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
