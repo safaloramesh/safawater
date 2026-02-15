72742cb224588b984a61ee5c3afefe86ba4fac8b
safaloramesh <safaloramesh@gmail.com>
2026-02-15 20:55:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRITICAL: Fixes the white screen by using relative paths
+  base: './', // CRITICAL: This adds the "." to asset paths to fix the white screen
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
